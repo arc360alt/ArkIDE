34ad63c3670a11b7a0ad0962ca11b52620ae9638
arc360alt <voidyt16@gmail.com>
2024-12-18 16:41:14 -0600

    [skip ci] Build for fca3aba2d1ca6f0d226f4c8af381ceb9aa3abaf7

--- js/credits.js
+++ js/credits.js
@@ -274,13 +274,10 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "guiColors", function() { return guiColors; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "blockColors", function() { return blockColors; });
 const guiColors = {
-  'motion-primary': 'hsla(260, 60%, 60%, 1)',
-  'motion-primary-transparent': 'hsla(260, 60%, 60%, 0.9)',
-  'motion-tertiary': 'hsla(260, 42%, 51%, 1)',
-  'looks-secondary': 'hsla(260, 60%, 60%, 1)',
-  'looks-transparent': 'hsla(260, 60%, 60%, 0.35)',
-  'looks-light-transparent': 'hsla(260, 60%, 60%, 0.15)',
-  'looks-secondary-dark': 'hsla(260, 42%, 51%, 1)'
+  'looks-secondary': 'hsla(215, 100%, 65%, 1)',
+  'looks-transparent': 'hsla(215, 100%, 65%, 0.35)',
+  'looks-light-transparent': 'hsla(215, 100%, 65%, 0.15)',
+  'looks-secondary-dark': 'hsla(215, 60%, 50%, 1)'
 };
 const blockColors = {};
 
@@ -299,25 +296,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "guiColors", function() { return guiColors; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "blockColors", function() { return blockColors; });
 const guiColors = {
-  'motion-primary': '#ff4c4c',
-  'motion-primary-transparent': '#ff4c4ce6',
-  'motion-tertiary': '#cc3333',
-  'looks-secondary': '#ff4c4c',
-  'looks-transparent': '#ff4d4d59',
-  'looks-light-transparent': '#ff4d4d26',
-  'looks-secondary-dark': 'hsla(0, 42%, 51%, 1)',
-  'extensions-primary': 'hsla(10, 85%, 65%, 1)',
-  'extensions-tertiary': 'hsla(10, 85%, 40%, 1)',
-  'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
-  'extensions-light': 'hsla(10, 57%, 85%, 1)',
-  'drop-highlight': '#ff8c8c',
-  // eslint-disable-next-line max-len
-  'menu-bar-background-image': 'linear-gradient(90deg, rgba(255, 0, 0, 0.75) 0%, rgba(255, 154, 0, 0.75) 10%, rgba(208, 222, 33, 0.75) 20%, rgba(79, 220, 74, 0.75) 30%, rgba(63, 218, 216, 0.75) 40%, rgba(47, 201, 226, 0.75) 50%, rgba(28, 127, 238, 0.75) 60%, rgba(95, 21, 242, 0.75) 70%, rgba(186, 12, 248, 0.75) 80%, rgba(251, 7, 217, 0.75) 90%, rgba(255, 0, 0, 0.75) 100%)'
+  'looks-secondary': 'hsla(215, 100%, 65%, 1)',
+  'looks-transparent': 'hsla(215, 100%, 65%, 0.35)',
+  'looks-light-transparent': 'hsla(215, 100%, 65%, 0.15)',
+  'looks-secondary-dark': 'hsla(215, 60%, 50%, 1)'
 };
-const blockColors = {
-  checkboxActiveBackground: '#ff4c4c',
-  checkboxActiveBorder: '#cc3333'
-};
+const blockColors = {};
 
 
 /***/ }),
@@ -334,23 +318,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "guiColors", function() { return guiColors; });
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "blockColors", function() { return blockColors; });
 const guiColors = {
-  'motion-primary': '#ff4c4c',
-  'motion-primary-transparent': '#ff4c4ce6',
-  'motion-tertiary': '#cc3333',
-  'looks-secondary': '#ff4c4c',
-  'looks-transparent': '#ff4d4d59',
-  'looks-light-transparent': '#ff4d4d26',
-  'looks-secondary-dark': 'hsla(0, 42%, 51%, 1)',
-  'extensions-primary': 'hsla(10, 85%, 65%, 1)',
-  'extensions-tertiary': 'hsla(10, 85%, 40%, 1)',
-  'extensions-transparent': 'hsla(10, 85%, 65%, 0.35)',
-  'extensions-light': 'hsla(10, 57%, 85%, 1)',
-  'drop-highlight': '#ff8c8c'
+  'looks-secondary': 'hsla(215, 100%, 65%, 1)',
+  'looks-transparent': 'hsla(215, 100%, 65%, 0.35)',
+  'looks-light-transparent': 'hsla(215, 100%, 65%, 0.15)',
+  'looks-secondary-dark': 'hsla(215, 60%, 50%, 1)'
 };
-const blockColors = {
-  checkboxActiveBackground: '#ff4c4c',
-  checkboxActiveBorder: '#cc3333'
-};
+const blockColors = {};
 
 
 /***/ }),
